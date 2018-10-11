1106234546a39fde65aa124136c600e72bd34e65
Nick Randell <nick.randell@sceneskope.com>
2018-10-11 06:15:49 +0100

    Fixed some missing files and build error

---
+++ AppConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleHostingConsole
+{
+    public class AppConfig
+    {
+        public string TextToPrint { get; set; }
+    }
+}
